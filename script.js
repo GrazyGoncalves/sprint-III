56dea2d6c2be4e1814928701b5734a9a223ddf91
Graziella Gonçalves <127903530+GrazyGoncalves@users.noreply.github.com>
2023-07-19 03:24:46 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const buttonHeader = document.getElementById('button-header')
-const navbarNav = document.getElementById('navbarNav')
-
-function mostrarOcultar(){
-  navbarNav.classList.toggle('show')
-}
-
-buttonHeader.addEventListener('click', mostrarOcultar)
